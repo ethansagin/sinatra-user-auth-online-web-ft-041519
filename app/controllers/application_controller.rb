d30c5e4910e1cd54e52245453182f43e42cc6909
Ethan Sagin <ethansagin@gmail.com>
2019-06-07 22:04:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,6 @@ class ApplicationController < Sinatra::Base
 
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    binding.pry
     @user.save
     session[:user_id] = @user.id
 
